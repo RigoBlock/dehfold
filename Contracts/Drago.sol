1b821f0a7a7cc047a451e9799b11b0972959958d
escursionista <gabriele@rigoinvestment.com>
2016-07-24 20:22:09 +0200

    Update Drago.sol

--- Contracts/Drago.sol
+++ Contracts/Drago.sol
@@ -1,1 +1,19 @@
+import "Dragowned.sol";
 
+contract Drago is Dragowned { 
+    
+    uint256 public totalSupply;
+    
+    function balanceOf(address _Dragowner) constant returns (uint256 balance);
+
+    function setPrices(uint256 newSellPrice, uint256 newBuyPrice);
+    
+    function buy(address _from, uint _value);
+    
+    function sell(uint amount, address _to);
+    
+    //function balanceOf(address _Dragowner);
+    
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    
+}
