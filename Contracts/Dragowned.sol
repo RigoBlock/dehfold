70eea360e7b4c55b80a1f22503ef788f914606c3
escursionista <gabriele@rigoinvestment.com>
2016-07-24 20:21:37 +0200

    Update Dragowned.sol

--- Contracts/Dragowned.sol
+++ Contracts/Dragowned.sol
@@ -1,1 +1,16 @@
-/*
+contract Dragowned {
+    address public Dragowner;
+
+    function Dragowned() {
+        Dragowner = tx.origin;
+    }
+
+    modifier onlyDragowner {
+        if (msg.sender == Dragowner)
+        _
+    }
+
+    function transferDragownership(address newDragowner) onlyDragowner {
+        Dragowner = newDragowner;
+    }
+}
