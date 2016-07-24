428105d4286e8dcb0949010dc4765e73a675d141
escursionista <gabriele@rigoinvestment.com>
2016-07-24 20:22:54 +0200

    Create HumanStandardDrago.sol

---
+++ Contracts/HumanStandardDrago.sol
@@ -0,0 +1,20 @@
+import "StandardDrago.sol";
+
+contract HumanStandardDrago is StandardDrago {
+    
+    string public name;
+    uint8 public decimals;
+    string public symbol;
+    string public version = 'H0.1';
+    
+    function HumanStandardDrago(string _dragoName,  string _dragoSymbol/*, uint8 _decimalUnits*/) {
+        name = _dragoName;    
+        symbol = _dragoSymbol;
+        //decimals = _decimalUnits;
+    }
+    
+    function() {
+		throw;
+    }
+	
+}
