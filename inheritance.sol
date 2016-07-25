f7d63e55850696850286de168cbb0cd9b7e9ebef
escursionista <gabriele@rigoinvestment.com>
2016-07-25 10:58:19 +0200

    Create inheritance.sol

---
+++ inheritance.sol
@@ -0,0 +1,33 @@
+// This gist explains some misunderstandings about Solidity constructors parameters and inheritance
+
+// Base contract to extend
+contract BaseContract {
+    function BaseContract(bool myVar) {}
+}
+
+// Wrong way to extend a contract that use parameters in its own constructor
+contract ExtendedContract_WrongConstructor is BaseContract {
+    bool _myVar;
+    function ExtendedContract_WrongConstructor(bool myVar) {
+        _myVar = myVar;
+    }
+}
+
+// Right way to extend a contract that use parameters in its own constructor
+contract ExtendedContract_CorrectConstructor is BaseContract {
+    bool _myVar;
+    function ExtendedContract_CorrectConstructor(bool myVar) BaseContract(myVar){
+        _myVar = myVar;
+    }
+}
+
+// Test the two cases
+contract Tester {
+    function Tester(){
+        //This does not compile
+        //ExtendedContract_WrongConstructor myContract = new ExtendedContract_WrongConstructor(true); //This throws: "Error: Trying to create an instance of an abstract contract."
+        
+        //This compiles
+        ExtendedContract_CorrectConstructor myCorrectContract = new ExtendedContract_CorrectConstructor(true);
+    }
+}
