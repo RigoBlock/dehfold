aa4cf41bc15da1df3e14d5f2d0005fb2ce119a20
escursionista <gabriele@rigoinvestment.com>
2016-12-04 16:27:03 +0100

    Update HF.sol

--- HF.sol
+++ HF.sol
@@ -1,22 +1,7 @@
-//GNU GENERAL PUBLIC LICENSE
-//sol HF
-//contract for investment fund.
-// @authors:
-//    Gabriele Rigo <gabriele.rigo@gmail.com>
-// Copyright 2016 Gabriele Rigo.
-
-// HF is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// HF is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with HF.  If not, see <http://www.gnu.org/licenses/>.
+// Copyright 2016 Gabriele Rigo
+// Investment Vehicle contract.
+// by Gabriele Rigo <gabriele@rigoinvestment.com>
+// Released under the Apache Licence 2.
 
 contract owned {
     address public owner;
