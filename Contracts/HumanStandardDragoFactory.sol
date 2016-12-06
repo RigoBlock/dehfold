d44f2f293c4c46e5cd95f4e9c02ba732b99e6b15
escursionista <gabriele@rigoinvestment.com>
2016-12-06 19:02:39 +0100

    minor styling amendmts

--- Contracts/HumanStandardDragoFactory.sol
+++ Contracts/HumanStandardDragoFactory.sol
@@ -9,20 +9,19 @@ import "HumanStandardDrago.sol";
 
 contract HumanStandardDragoFactory is DragoRegistry {
 	
-	string public version = 'DF0.1'; //alt uint public version = 1
-	uint[] _dragoID;
-	address[] newDragos;
+    string public version = 'DF0.1'; //alt uint public version = 1
+    uint[] _dragoID;
+    address[] newDragos;
 	
     event DragoCreated(string _name, address _drago, address Dragowner, uint _dragoID);
     
-	function HumanStandardDragoFactory () {
-	    
-	    }	
+    function HumanStandardDragoFactory () {	    
+	}	
 	
-	function createHumanStandardDrago(string _name, string _symbol) returns (address _drago, uint _dragoID) {
-		HumanStandardDrago newDrago = (new HumanStandardDrago(_name, _symbol));
-		newDragos.push(address(newDrago));
-		created[msg.sender].push(address(newDrago));
+    function createHumanStandardDrago(string _name, string _symbol) returns (address _drago, uint _dragoID) {
+	HumanStandardDrago newDrago = (new HumanStandardDrago(_name, _symbol));
+	newDragos.push(address(newDrago));
+	created[msg.sender].push(address(newDrago));
         newDrago.transferDragownership(tx.origin);
         register(_drago, _dragoID);
         DragoCreated(_name, address(newDrago), tx.origin, uint(newDrago));
