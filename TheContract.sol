3c0fab1eef39a048e139ad341316a3a79fcbc7d7
escursionista <gabriele@rigoinvestment.com>
2017-04-05 14:34:28 +0200

    Update TheContract.sol

--- TheContract.sol
+++ TheContract.sol
@@ -1,3 +1,9 @@
+//! Library interaction contract.
+//! By Gabriele Rigo (Rigo Investment), 2017.
+//! Released under the Apache Licence 2.
+
+pragma solidity ^0.4.8;
+
 pragma solidity ^0.4.8;
 
 import "./LibInterface.sol";
